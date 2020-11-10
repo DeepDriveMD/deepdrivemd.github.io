9b7c6c32d868dfefc47cd6c517bd615670c2f71b
braceal <braceal@umich.edu>
2020-11-09 22:37:06 -0500

    clean up

--- _site/README.md
+++
@@ -1,1 +0,0 @@
-# deepdrivemd.github.io
--- _site/init.sh
+++
@@ -1,7 +0,0 @@
-echo "# deepdrivemd.github.io" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:DeepDriveMD/deepdrivemd.github.io.git
-git push -u origin main
